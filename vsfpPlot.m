bf662bd29f3e816672473104533452a7ed670d36
amorrissette <amoriss@emory.edu>
2015-07-08 14:29:28 -0400

    added vsfpPlot.m

---
+++ vsfpPlot.m
@@ -0,0 +1,9 @@
+function outTraces = vsfpPlot(vsfp_data, numDiv, plotOn)
+
+% Generates example traces and plots from region averages based on number of
+% divisions of vsfp imaging area
+%
+%   Usage outTraces = vsfpPlot(vsfp_data, numDiv, plotOn)
+%
+%   * Must use divisions that play nicely with 100 (eg 25,50,5,10,etc)
+
