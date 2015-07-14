5b179c32e83859c85fc8fcb86a6ce4044552cfed
amorrissette <amoriss@emory.edu>
2015-07-14 10:43:26 -0400

    updates to averaging functions

---
+++ roiAvg.m
@@ -0,0 +1,6 @@
+function avgROI = roiAvg(roi_data, numDiv)
+
+% Function for averaging ROI data to plot with output of vsfpAvg.m script
+%
+% Usage: 
+%   avgROI = roiAvg(roi_data, numDiv);
--- vsfpAvg.m
+++ vsfpAvg.m
@@ -7,12 +7,15 @@ function avgVSFP = vsfpAvg(vsfp_data, numDiv)
 %
 %   * Must use divisions that play nicely with 100 (eg 25,50,5,10,etc)
 
-%% create temp data for faster operations 
+%% 
+% if strcmp(plotOn, 'on') == 1 
+%     p = 1;
+% else p = 0;
+% end
 
 [d1,~,d3] = size(vsfp_data);
 
 totalDiv = floor(d1/numDiv);
-I = zeros(100,100,totalDiv);
 avgVSFP = zeros(totalDiv, totalDiv, d3);
 %
 % if mod(numDiv,d1) ~= 0 
@@ -21,19 +24,26 @@ avgVSFP = zeros(totalDiv, totalDiv, d3);
 
 %% Divide and Conquer
 
-% Assuming 9 divisions 20x20
 tic
 i = 1;
+figure
 for x = 1:totalDiv
     for y = 1:totalDiv
         avgVSFP(x,y,1:d3) = mean(mean(vsfp_data(numDiv*(x-1)+1:(numDiv*x),numDiv*(y-1)+1:(numDiv*y),:)));
-%         I(numDiv*(x-1)+1:(numDiv*x),numDiv*(y-1)+1:(numDiv*y),i) = ones(numDiv);
-%         i = i+1;
-%         for z = 1:d3
-%             avgVSFP(x,y,z) = mean(vsfp_data(:,:,z).*I(1:100,1:100,i));
-%         end
+        subplot(totalDiv, totalDiv, i);
+        plot(squeeze(avgVSFP(x,y,1:d3)));
+        axis off
+        i = i+1;
     end
 end
 toc
 
 
+% old stuff
+
+%         I(numDiv*(x-1)+1:(numDiv*x),numDiv*(y-1)+1:(numDiv*y),i) = ones(numDiv);
+%         for z = 1:d3
+%             avgVSFP(x,y,z) = mean(vsfp_data(:,:,z).*I(1:100,1:100,i));
+%         end
+
+% I = zeros(100,100,totalDiv);
